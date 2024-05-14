734e294d7d703eeea94f92a11f751f9a145f8fb1
Ashutosh Prasad <ashutoshprasad2427@gmail.com>
2024-05-14 11:35:02 +0530

    initial commit with Ready Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "../App.css";
+const Navbar = () => {
+  return (
+    <nav>
+      <img src="images/Frame 2 1.png" alt="" />
+      <ul>
+        <li>
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            home
+          </a>
+        </li>
+        <li>
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            about
+          </a>
+        </li>
+        <li>
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            services
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
